84586948c06d81a6b6cb0786b7eefee08f2d5320
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2022-06-24 16:07:09 +0200

    feat: parse text from image in container

--- TeasseractOCROnLInux/Program.cs
+++ TeasseractOCROnLInux/Program.cs
@@ -1,4 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using TesseractOCR;
+using TesseractOCR.Enums;
 
+var testImagePath = "./in/scan1.png";
+using var engine = new Engine(@"./tessdata", Language.English, EngineMode.Default);
+using var img = TesseractOCR.Pix.Image.LoadFromFile(testImagePath);
+using var page = engine.Process(img);
+Console.WriteLine("Mean confidence: {0}", page.MeanConfidence);
+Console.WriteLine("Text: \r\n{0}", page.Text);
